d2fbd17a1837ebcb3358c461faa122d40bec8be2
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-05-30 09:34:45 +0000

    Modularize Parser

--- main.go
+++ main.go
@@ -20,41 +20,45 @@ func main() {
 	data := extractor.GetFeedURL(file)
 
 	for _, FeedURL := range data {
-		fp := gofeed.NewParser()
-
-		feed, _ := fp.ParseURL(FeedURL)
-
-		for _, item := range feed.Items {
-
-			NowTime := time.Now()
-			ParsedNowTime := time.Unix(NowTime.Unix(), 0)
-
-			PublishedTime := item.PublishedParsed
-			ParsedPublishedTime := time.Unix(PublishedTime.Unix(), 0)
-
-			if ParsedNowTime.Sub(ParsedPublishedTime).Hours() < 6 {
-
-				PostTitle := item.Title
-				PostLink := item.Link
-				PostAuthor := item.Author.Name
-				Category := item.Categories
-
-				hashtags, _ := GetHastags(Category)
-
-				Tweet := fmt.Sprintf("%s was published by %s  🎉🎉🎉 \n %s \n %s", PostTitle, PostAuthor, hashtags, PostLink)
-
-				TweeetId, _ := platform.PublishToTwitter(Tweet)
-
-				fmt.Printf("%s with the TweetID: %s was posted! ✔️ \n ", PostTitle, TweeetId)
-
-			}
-
-		}
+		postParser(FeedURL)
 	}
 
 }
 
-func GetHastags(Categories []string) (string, error) {
+func postParser(FeedURL string) {
+	fp := gofeed.NewParser()
+
+	feed, _ := fp.ParseURL(FeedURL)
+
+	for _, item := range feed.Items {
+
+		NowTime := time.Now()
+		ParsedNowTime := time.Unix(NowTime.Unix(), 0)
+
+		PublishedTime := item.PublishedParsed
+		ParsedPublishedTime := time.Unix(PublishedTime.Unix(), 0)
+
+		if ParsedNowTime.Sub(ParsedPublishedTime).Hours() < 24 {
+
+			PostTitle := item.Title
+			PostLink := item.Link
+			PostAuthor := item.Author.Name
+			Category := item.Categories
+
+			hashtags, _ := getHastags(Category)
+
+			Tweet := fmt.Sprintf("%s was published by %s  🎉🎉🎉 \n %s \n %s", PostTitle, PostAuthor, hashtags, PostLink)
+
+			TweeetId, _ := platform.PublishToTwitter(Tweet)
+
+			fmt.Printf("%s with the TweetID: %s was posted! ✔️ \n ", PostTitle, TweeetId)
+
+		}
+
+	}
+}
+
+func getHastags(Categories []string) (string, error) {
 
 	var Category string
 
